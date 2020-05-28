b3442ae8ed8105669b7ed3c1fb7787d97a5c49f9
Janis Vaneylen <janis.vaneylen@gmail.com>
2020-05-28 14:35:13 +0200

    added model, controller, pundit, and more

---
+++ test/controllers/table_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class TableControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get table_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get table_show_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get table_create_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get table_new_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get table_update_url
+    assert_response :success
+  end
+
+end
