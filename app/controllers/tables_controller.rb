8d6507b28fa1b59135c1867c5b6d1fae717b30d7
Janis Vaneylen <janis.vaneylen@gmail.com>
2020-05-28 14:35:13 +0200

    added model, controller, pundit, and more

---
+++ app/controllers/tables_controller.rb
@@ -0,0 +1,45 @@
+class TablesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:home, :index, :show]
+
+  def index
+    @tables = Table.all
+  end
+
+  def show
+    @table = Table.find(params[:id])
+  end
+
+  def create
+    @table = Table.new(table_params)
+    @table.save
+    redirect_to table_path(@table)
+  end
+
+  def new
+    @table = Table.new
+  end
+
+  def edit
+    @table = Table.find(params[:id])
+  end
+
+  def update
+    @table = Table.find(params[:id])
+    @table.update(table_params)
+
+    redirect_to table_path(@table)
+  end
+
+  def destroy
+    @table = Table.find(params[:id])
+    @table.destroy
+
+    redirect_to tables_path
+  end
+
+  private
+
+  def table_params
+    params.require(:table).permit(:description, :location)
+  end
+end
