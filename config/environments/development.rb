75f2e5913af5772e95fbd6978926d9fe5d2d6d6b
Janis4411 <janis.vaneylen@gmail.com>
2022-01-28 16:15:05 +0100

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,8 +9,8 @@ Rails.application.configure do
     domain: "example.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD']
   }
 
   # In the development environment your application's code is reloaded on
