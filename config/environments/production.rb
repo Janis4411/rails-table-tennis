421a627fb017f8f4b6e3fbd8bb39cf79b22dd1b8
Janis4411 <janis.vaneylen@gmail.com>
2022-01-28 12:35:59 +0100

    changed some devise credentials in production.rb, still trying to fix forget password bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = { :host => 'http://berlintabletennismap.com' }
+
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
@@ -8,10 +10,10 @@ Rails.application.configure do
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
-    password:ENV["GMAIL_PASSWORD"]
+    password: ENV["GMAIL_PASSWORD"]
   }
   # doesn't have to be Heroku, but you get the idea.
-  config.action_mailer.default_url_options = { :host => 'berlintabletennismap.com' }
+
 
   # Code is not reloaded between requests.
   config.cache_classes = true
